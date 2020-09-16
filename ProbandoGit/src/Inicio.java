51d228a6e29e36fc003028bded8770650ad1c33a
juan jimenez <sajcjm@hotmail.com>
2020-09-16 15:10:29 +0200

    segunda clase saludo

--- ProbandoGit/src/Inicio.java
+++ ProbandoGit/src/Inicio.java
@@ -4,6 +4,9 @@ public class Inicio {
 	public static void main(String[] args) {
 		System.out.println("probando GIT");
 
+		Saludo saludo=new Saludo();
+		saludo.setSaludo("hola que tal estas");
+		System.out.println(saludo.getSaludo());
 	}
 
 }
---
+++ ProbandoGit/src/Saludo.java
@@ -0,0 +1,15 @@
+
+public class Saludo {
+	
+	private String saludo;
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	
+
+}
